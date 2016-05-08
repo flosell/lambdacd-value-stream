3776191143cd6b83145afdcbf634a8cd24895b9b
Florian Sellmayr <florian.sellmayr@gmail.com>
2016-05-08 12:38:06 +0200

    mock out example pipelines to sketch out future usage

--- example/lambdacd_value_stream/example/simple_pipeline.clj
+++ example/lambdacd_value_stream/example/simple_pipeline.clj
@@ -4,6 +4,7 @@
             [lambdacd.steps.manualtrigger :refer [wait-for-manual-trigger]]
             [lambdacd.steps.control-flow :refer [either with-workspace in-parallel run]]
             [lambdacd.core :as lambdacd]
+            [lambdacd-value-stream.core :as value-stream]
             [ring.server.standalone :as ring-server]
             [lambdacd.ui.ui-server :as ui]
             [lambdacd-git.core :as git]
@@ -11,37 +12,64 @@
             [clojure.java.io :as io]
             [lambdacd.util :as utils]))
 
-(def repo "git@github.com:flosell/testrepo")
+(def lambdacd-repo "git@github.com:flosell/lambdacd")
+(def lambdacd-git-repo "git@github.com:flosell/lambdacd-git")
 
-(defn wait-for-git [args ctx]
-  (git/wait-for-git ctx repo
+(defn wait-for-lambdacd-commit [args ctx]
+  (git/wait-for-git ctx lambdacd-repo
                     :ref "refs/heads/master"
                     :ms-between-polls (* 60 1000)))
 
-(defn clone [args ctx]
-  (git/clone ctx repo (:revision args) (:cwd args)))
+(defn clone-lambdacd [args ctx]
+  (git/clone ctx lambdacd-repo (:revision args) (:cwd args)))
 
-(defn ls [args ctx]
-  (shell/bash ctx (:cwd args) "ls"))
+(defn clone-lambdacd-git [args ctx]
+  (git/clone ctx lambdacd-git-repo "refs/heads/master" (:cwd args)))
 
-(def pipeline-structure
+(defn test-lambdacd [args ctx]
+  (shell/bash ctx (:cwd args) "./go test"))
+
+(defn test-lambdacd-git [args ctx]
+  (shell/bash ctx (:cwd args) "./go test"))
+
+(defn release-lambdacd [args ctx]
+  (shell/bash ctx (:cwd args) "./go release-local"))
+
+(defn wait-for-lambdacd-pipeline [args ctx]
+  (value-stream/wait-for-pipline-success :lambdacd ctx))
+
+(def lambdacd-pipeline-structure
   `((either
       wait-for-manual-trigger
-      wait-for-git)
+      wait-for-lambdacd-commit)
      (with-workspace
-       clone
-       git/list-changes
+       clone-lambdacd
+       test-lambdacd
+       release-lambdacd)))
 
-       ls)))
+(def lambdacd-git-pipeline-structure
+  `((either
+      wait-for-manual-trigger
+      wait-for-lambdacd-pipeline)
+     (with-workspace
+       clone-lambdacd-git
+       test-lambdacd-git)))
 
 (defn -main [& args]
-  (let [home-dir (utils/create-temp-dir)
-        config   {:home-dir home-dir}
-        pipeline (lambdacd/assemble-pipeline pipeline-structure config)]
+  (let [home-dir              (utils/create-temp-dir)
+        lambdacd-pipeline     (lambdacd/assemble-pipeline lambdacd-pipeline-structure {:home-dir home-dir})
+        lambdacd-git-pipeline (lambdacd/assemble-pipeline lambdacd-git-pipeline-structure {:home-dir (utils/create-temp-dir)})
+        lambdacd-ui           (ui/ui-for lambdacd-pipeline)
+        lambdacd-git-ui       (ui/ui-for lambdacd-git-pipeline)
+        ]
     (git/init-ssh!)
-    (runners/start-one-run-after-another pipeline)
+    (runners/start-one-run-after-another lambdacd-pipeline)
+    (runners/start-one-run-after-another lambdacd-git-pipeline)
     (ring-server/serve (routes
-                         (ui/ui-for pipeline)
-                         (git/notifications-for pipeline))
+                         (context "/lambdacd" []
+                           lambdacd-ui)
+                         (context "/lambdacd-git" []
+                           lambdacd-git-ui)
+                         )
                        {:open-browser? false
                         :port          8083})))
--- src/lambdacd_value_stream/core.clj
+++ src/lambdacd_value_stream/core.clj
@@ -1,7 +1,22 @@
-(ns lambdacd-value-stream.core)
+(ns lambdacd-value-stream.core
+  (:require [lambdacd.steps.support :as support]
+            [clojure.core.async :as async]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!")
   x)
+
+
+(defn wait-for-pipline-success [pipeline ctx]
+  (support/capture-output ctx
+    (do
+      (println "NOT IMPLEMENTED YET")
+      (println "Will wait forever. But one day it'll wait until pipeline" pipeline "finished successfully")
+      (loop []
+        (support/if-not-killed ctx
+          (do
+            (async/>!! (:result-channel ctx) [:status :waiting])
+            (Thread/sleep (* 1 1000))
+            (recur)))))))
